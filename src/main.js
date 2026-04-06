3130ed66be9128491babf0be20bebcbebe4246cc
virtu0427 <jdi04278@gmail.com>
2026-04-06 16:53:53 +0900

    Create src/main.js with Vue app mount code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Main entry point of the Vue application
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
